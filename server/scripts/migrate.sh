b15ae11cf786aeb6c8814a7ef9d8c7da61788b40
schweigenderflugel <facundoezcastro@gmail.com>
2025-01-13 13:29:01 -0300

    add changes to the scripts files

--- server/scripts/migrate.sh
+++ server/scripts/migrate.sh
@@ -1,5 +1,8 @@
+# Borra los archivos de migraciones
 rm -rf prisma/migrations
 
+# Resetea la base de datos
 npx prisma migrate reset --force --skip-seed
 
+# Ejecuta las migraciones a la base de datos
 npx prisma migrate dev --name init --skip-seed
