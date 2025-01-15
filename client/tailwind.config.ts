34811db94df53657faa977aad3a89f9aa34c11e7
Wkatir <110741874+wkatir@users.noreply.github.com.>
2025-01-14 18:35:21 -0600

    feat(auth): setup auth folder structure & theme colors

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -11,6 +11,10 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        slate: "#E5E7EB",        // Para elementos grises claros
+        copper: "#F97316",       // Para el botón marrón/cobrizo de registro
+        teal: "#239BC3",         // Para el azul verdoso del logo/fondo
+        shadow: "#0F1417",       // Para sombras y textos oscuros
       },
     },
   },
