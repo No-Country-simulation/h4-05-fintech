8a99c15cd76c235a41b1340106cf83f14a1c6659
schweigenderflugel <facundoezcastro@gmail.com>
2025-01-15 15:03:16 -0300

    fix typos errors

--- server/src/modules/auth/auth.controller.ts
+++ server/src/modules/auth/auth.controller.ts
@@ -36,9 +36,9 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('registry')
-  @ApiOperation({ summary: 'Resgister a new user' })
+  @ApiOperation({ summary: 'Register a new user' })
   @ApiBody({ type: RegistryDto, required: true })
-  @ApiBadRequestResponse({ description: `incoming data is invalid, or the passwords don't match` })
+  @ApiBadRequestResponse({ description: `Incoming data is invalid, or the passwords don't match` })
   @ApiConflictResponse({ description: 'User already registered' })
   @ApiCreatedResponse(RegistrySuccess)
   @ApiInternalServerErrorResponse({ description: 'Unexpected server error' })
@@ -48,7 +48,7 @@ export class AuthController {
 
   @Get('verify/:code')
   @ApiOperation({ summary: 'Verify registered user' })
-  @ApiParam({ name: 'code', description: 'must be a valid 32-digit hex code', required: true })
+  @ApiParam({ name: 'code', description: 'Must be a valid 32-digit hex code', required: true })
   @ApiNotFoundResponse({ description: 'User not found' })
   @ApiNotAcceptableResponse({ description: 'Invalid 32-digit hex code' })
   @ApiOkResponse(VerifySuccess)
