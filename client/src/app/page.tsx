032202347b84fcd9559ddc9cb69af4112e0912e2
hackermate <villacresa@gmail.com>
2025-01-16 07:25:08 -0500

    fornt-register

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,42 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+const Home = () => {
   return (
-    <div>
-  
-    </div>
+    <main className="min-h-screen flex flex-col items-center justify-center mt-10">
+      <div className="w-full max-w-md space-y-6">
+        <div className="flex flex-col items-center">
+          <Image
+            src="https://res.cloudinary.com/di0cvbfdb/image/upload/v1736977179/fintech/yzuwrtmgdvqbqjvgplcw.svg"
+            alt="Logo"
+            width={342}
+            height={50}
+            priority
+          />
+          
+        </div>
+        <Card className="shadow-none border-none mt-30">
+            <div className="">
+              <Link href="/auth/login">
+                <Button className="w-full h-[52px] bg-[#8D4E2A33] text-[#BDE9FF] text-base font-normal tracking-wide">
+                Iniciar sesión
+                </Button>
+              </Link>
+            </div>
+
+            <div className="mt-5">
+              <Link href="/auth/register">
+                <Button className="w-full h-[52px] bg-gradient-to-r from-yellow-950 via-amber-900 to-zinc-600 text-[#BDE9FF] text-base font-normal tracking-wide hover:bg-gradient-to-r hover:from-yellow-950 hover:via-gray-800 hover:to-zinc-600">
+                  Registrarme
+                </Button>
+               </Link>
+            </div>
+        </Card>
+      </div>
+    </main>
   );
-}
+};
+
+export default Home;
