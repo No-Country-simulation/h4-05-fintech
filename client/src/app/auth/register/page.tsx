d42392891460d2883a10cd0b2b78ce7689f7c0ea
wkatir <wilmerhenrysalazarmartinez@gmail.com>
2025-01-16 12:58:50 -0600

    feat: fixing a ts issue with a component

--- client/src/app/auth/register/page.tsx
+++ client/src/app/auth/register/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { toast} from "nextjs-toast-notify";
