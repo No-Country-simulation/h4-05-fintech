978a219e0c87c802876812166afeccf6bf5307cf
hackermate <villacresa@gmail.com>
2025-01-17 20:55:31 -0500

    frontt-log

--- client/src/components/auth/Login.tsx
+++ client/src/components/auth/Login.tsx
@@ -1,11 +1,11 @@
-"use client";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
 import axios from "axios";
-const login: string = process.env.LOGIN as string;
+
+const login: string = import.meta.env.VITE_LOGIN as string;
 
 const LoginPage = () => {
   const [formData, setFormData] = useState({
--- client/src/components/auth/Register.tsx
+++ client/src/components/auth/Register.tsx
@@ -5,7 +5,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
 import axios from "axios";
-const registry: string = process.env.REGISTRY as string;
+const registry: string = import.meta.env.VITE_REGISTRY as string;
 
 interface formValues {
   email: string;
