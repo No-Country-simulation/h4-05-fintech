d0e48ec4dea83a4599c9a9fd56ff5e18401126a6
hackermate <villacresa@gmail.com>
2025-01-19 20:12:10 -0500

    frontt-log

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -4,6 +4,8 @@ interface ImportMetaEnv {
     VITE_LOGIN: string;
     VITE_REGISTRY: string;
     VITE_LOGOUT: string;
+    VITE_VERIFY: string;
+    VITE_PORT: string;
 }
 
 interface ImportMeta {
