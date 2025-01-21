82aed0bfeb6646980adb90427bc58b1607230cb3
Alex Marin Mendez <alexmarinmendez@gmail.com>
2025-01-21 14:43:33 -0500

    Add logout functionality and navigate to authentication page on logout

--- client/src/components/profile/Dashboard.tsx
+++ client/src/components/profile/Dashboard.tsx
@@ -1,24 +1,28 @@
 import { instance } from "@/api/axios";
+import { useNavigate } from "react-router";
 
 const Dashboard = () => {
-const handleLogout = async () => {
-    console.log("Logout");
-    
-        try {
-          await instance.get(
-            '/auth/logout',
-            {
-              withCredentials: true,
-              headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Credentials': 'true',
-              }
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        console.log("Logout");
+        
+            try {
+            await instance.get(
+                '/auth/logout',
+                {
+                withCredentials: true,
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Credentials': 'true',
+                }
+                }
+            );
+            navigate('/auth');
+            } catch (error: any) {
+            console.error(error);
             }
-          );
-        } catch (error: any) {
-          console.error(error);
-        }
-}
+    }
 
   return (
     <div>
