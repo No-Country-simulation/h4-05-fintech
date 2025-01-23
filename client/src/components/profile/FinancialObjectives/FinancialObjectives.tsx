6c550e6bf28a52737b922406304bcf0e7f13590f
Alex Marin Mendez <alexmarinmendez@gmail.com>
2025-01-23 11:58:04 -0500

    update FinancialObjectives component to use Link for navigation

--- client/src/components/profile/FinancialObjectives/FinancialObjectives.tsx
+++ client/src/components/profile/FinancialObjectives/FinancialObjectives.tsx
@@ -1,16 +1,18 @@
 import Button from "./Button"
 import Logo from "./Logo"
 import './FinancialObjectives.css'
+import { Link } from "react-router-dom"
 
 const FinancialObjectives = () => {
   return (
     <div className="financial-objectives">
+      <div></div>
         <div className="financial-objectives-title">
             <Logo />
         </div>
-        <div className="financial-objectives-button">
+        <Link to="/experience" className="financial-objectives-button">
             <Button />
-        </div>
+        </Link>
     </div>
   )
 }
