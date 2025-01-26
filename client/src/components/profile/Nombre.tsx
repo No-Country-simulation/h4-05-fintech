4b317ef5b352857b211f8f9f7e7117cbb0677dfa
hackermate <villacresa@gmail.com>
2025-01-26 16:08:01 -0500

    front-perfil

--- client/src/components/profile/Nombre.tsx
+++ client/src/components/profile/Nombre.tsx
@@ -1,50 +1,54 @@
 import { useState } from "react";
 import { Card } from "../ui/card";
-import { Link } from "react-router-dom";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Button } from "../ui/button";
 import axios from "axios";
 
+
 const Nombre = () => {
-    const [formData, setFormData] = useState({
-        name: "",
-        secondname: "",
-    });
+  const [formData, setFormData] = useState({
+    name: "",
+    secondname: "",
+  });
 
+  const [error, setError] = useState<string | null>(null);
 
-
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setError(null);
-        setFormData({ ...formData, [e.target.name]: e.target.value });
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formData);
+    try {
+      const response = await axios.post("http://localhost:4000", formData);
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        console.log(formData);
-        try {
-            const response = await axios.post("http://localhost:4000/api/auth/register", formData);
-            console.log(response);
-        } catch (error) {
-            console.log(error);
-        }
-          
-    };    
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setError(null);
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
   return (
     <main className="min-h-screen flex flex-col items-center justify-center">
       <div className="w-full max-w-md space-y-6">
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center mt-[11rem]" >
           <img
             src="https://res.cloudinary.com/di0cvbfdb/image/upload/v1736977179/fintech/yzuwrtmgdvqbqjvgplcw.svg"
             alt="Logo"
             className="w-[341px] h-[49px]"
           />
         </div>
-          <div className=" justify-end grid-rows-12">
-            <p className="text-[#88D0EF] text-center">Quien eres?</p>
-          </div>
-            <Card className=" border-none shadow-none">
-          <form onSubmit={handleSubmit} className="space-y-2 mt-[10rem]">
+        <div className=" justify-end grid-rows-12">
+          <p className="text-[#88D0EF] text-center">Quien eres?</p>
+        </div>
+        <Card className=" border-none shadow-none">
+          <form onSubmit={handleSubmit} className="space-y-2 mt-[5rem]">
             <div className="rounded-lg space-y-2 bg-[#11668233] p-3">
-              <Label htmlFor="name" className="text-[#8BD0EF]">
+              <Label htmlFor="name"
+                className="text-[#8BD0EF]"
+              >
                 Nombre
               </Label>
               <Input
@@ -59,8 +63,10 @@ const Nombre = () => {
               />
             </div>
             <div className="rounded-lg space-y-2 bg-[#11668233] p-3">
-              <Label htmlFor="password" className="text-[#8BD0EF]">
-                Contraseña
+              <Label htmlFor="apellido"
+                className="text-[#8BD0EF]"
+              >
+                Apellido
               </Label>
               <Input
                 id="secondname"
@@ -73,20 +79,10 @@ const Nombre = () => {
                 required
               />
             </div>
-              ? <Button type="submit" className="w-full h-[52px] bg-[#F9731633] text-[#BDE9FF] text-base font-normal tracking-wide" disabled={true}>
-                  Cargando...
-                </Button>
-              : <Button type="submit" className="w-full h-[52px] bg-[#F9731633] text-[#BDE9FF] text-base font-normal tracking-wide">
-                  Iniciar sesión
-                </Button>
+            <Button type="submit" className="w-full h-[52px] bg-[#F9731633] text-[#BDE9FF] text-base font-normal tracking-wide">
+              Continuar
+            </Button>
           </form>
-          <div className="mt-[8rem]">
-            <Link to="/auth/register">
-              <Button className="w-full h-[52px] bg-[#F9731633] text-[#BDE9FF] text-base font-normal tracking-wide">
-                Continuar
-              </Button>
-            </Link>
-          </div>
         </Card>
       </div>
     </main>
