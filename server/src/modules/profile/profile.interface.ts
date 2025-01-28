702f8ca61c8ba9445ac63fd814d93dec9bee7d49
schweigenderflugel <facundoezcastro@gmail.com>
2025-01-28 12:32:20 -0300

    fix errors on prisma

--- server/src/modules/profile/profile.interface.ts
+++ server/src/modules/profile/profile.interface.ts
@@ -3,3 +3,16 @@ export interface ICreateProfile {
   lastname: string;
   image: string;
 }
+
+export interface SendProfileToModel {
+  objetivo_financiero: string;
+  horizonte_tiempo: string;
+  conocimiento_inversiones: string;
+  formacion: string;
+  instrumentos_invertidos: string[];
+  reaccion_perdida: string;
+  fuente_ingresos: string;
+  ingresos_mensuales: string;
+  gastos_mensuales: string;
+  rango_ahorros: string;
+}
